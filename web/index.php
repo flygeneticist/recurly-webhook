2cecc258ff687647a3487b5169d61573cb9362a8
Kevin Keller <flygeneticist@gmail.com>
2015-05-06 11:20:21 +0900

    Added Recurly_PushNotification class.

--- web/index.php
+++ web/index.php
@@ -30,7 +30,45 @@ $app->post('/', function () use ($app) {
 
 $app->run();
 
-// HELPER FUNCTIONS
+// HELPER FUNCTIONS AND CLASSES
+class Recurly_PushNotification {
+	/* Notification type:
+	 *   [new_account updated_account canceled_account
+	 *    new_subscription updated_subscription canceled_subscription expired_subscription
+	 *    successful_payment failed_payment successful_refund void_payment]
+	 */
+	var $type;
+	var $account;
+	var $subscription;
+	var $transaction;
+	function __construct($post_xml) {
+		$this->parseXml($post_xml);
+	}
+
+	function parseXml($post_xml) {
+		if (!@simplexml_load_string($post_xml)) {
+			return;
+		}
+		$xml = new SimpleXMLElement($post_xml);
+
+		$this->type = $xml->getName();
+
+		foreach ($xml->children() as $child_node) {
+			switch ($child_node->getName()) {
+				case 'account':
+					$this->account = $child_node;
+					break;
+				case 'subscription':
+					$this->subscription = $child_node;
+					break;
+				case 'transaction':
+					$this->transaction = $child_node;
+					break;
+			}
+		}
+	}
+}
+
 function updateChartmogul($xml) {
 	echo "<h3>Building/Sending ChartMogul Request...</h3>";
 	$url     = 'https://app.chartmogul.com/api/events/recurly/DTWfFDlBFn2CJRkCeE0cbw';
